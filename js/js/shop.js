65963c4cf4dc0a0a67f1607eb7547a2ef0fea6f2
jokerfreak95 <freakanime061@gmail.com>
2025-10-02 14:43:41 +0300

    Update shop.js

--- js/js/shop.js
+++ js/js/shop.js
@@ -1,1 +1,14 @@
+fetch('data/products.json').then(r=>r.json()).then(draw);
 
+function draw(list){
+  const grid = document.getElementById('product-grid');
+  grid.innerHTML = list.map((p,i)=>`
+    <article class="card">
+      <a href="product.html?id=${p.id}">
+        <img src="${p.img||'images/placeholder.png'}" alt="${p.name}" />
+        <h4>${p.name}</h4>
+        <p>$${p.price}</p>
+      </a>
+      <button onclick='addItem(${JSON.stringify(p)})'>Quick Add</button>
+    </article>`).join('');
+}
